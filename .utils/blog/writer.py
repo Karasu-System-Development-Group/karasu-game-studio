f5d576724020acfb5c3ae9e6ce9380762dbe0cd7
Jorge <jorgejapao00@gmail.com>
2025-09-29 16:13:00 -0300

    new function to get right folder or create it added

--- .utils/blog/writer.py
+++ .utils/blog/writer.py
@@ -2,8 +2,29 @@ from bs4 import BeautifulSoup
 import datetime
 import os
 
+months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
 
-def definir_mes() -> str: 
+
+def current_folder() -> str:
+    ano: int = pegar_data()[1]
+    mes: int = int(pegar_data()[2])
+    mes_final = months[mes - 1]
+    final_path: str = os.path.join("pages", "devblog", str(ano), mes_final)
+
+    # Cria a pasta se não existir
+    if not os.path.exists(final_path):
+        os.makedirs(final_path)
+        print("Pasta criada!")
+    else:
+        print("A pasta já existe.")
+    return final_path
+        
+
+
+def pegar_data() -> str: 
     dt = datetime.datetime.now()
     ano: int = dt.year
     mes: int = dt.month
@@ -13,7 +34,7 @@ def definir_mes() -> str:
     if dia < 10:
         dia = f'0{dia}'
     data_formatada: str = (f'{dia}-{mes}-{ano}')
-    return data_formatada
+    return data_formatada, ano, mes, dia
 
 
 
@@ -44,8 +65,10 @@ def escrever_post(arquivo: str, conteudo: str, data: str):
 if __name__ == "__main__":
     continuar: bool = True
     contagem: int = 0
-    data: str = definir_mes()
+    data: str = pegar_data()[0]
     print(data)
+    pasta = current_folder()
+    arquivo_final = os.path.join(pasta, f"{data}.html")
     os.system('cls')
     print(f'Criando a postagem do dia {data}.')
     print(' ')
@@ -53,7 +76,7 @@ if __name__ == "__main__":
         if contagem == 0:
             arquivo: str = r".utils\blog\template.html"
         else:
-            arquivo: str = fr"{data}.html"
+            arquivo: str = arquivo_final
         conteudo: str = input(f'Escreva o {contagem+1}° parágrafo: ')
         escrever_post(arquivo, conteudo, data)
         contagem = contagem+1
